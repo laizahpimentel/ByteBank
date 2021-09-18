5671511a5a58673d16006e06eb59c6d4d2c9f7f1
laizahpimentel <laizahpimentel@gmail.com>
2021-09-17 21:48:08 -0300

    issues de cliente

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 1112223309;
+const cliente1Agencia =  1001;
+const cliente1Saldo = 0;
+
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 8882223309;
+const cliente2Agencia =  1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
