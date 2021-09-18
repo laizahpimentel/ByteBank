0700350fb398b1ba82ffd91ef274b2fe04f8b19d
laizahpimentel <laizahpimentel@gmail.com>
2021-09-17 23:31:53 -0300

    criando class

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
 
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 1112223309;
-const cliente1Agencia =  1001;
-const cliente1Saldo = 0;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+const cliente3 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 1112223309;
+cliente1.agencia =  1001;
+cliente1.saldo = 0;
+cliente1.rg = 123456789;
 
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 8882223309;
-const cliente2Agencia =  1001;
-const cliente2Saldo = 0;
+cliente2.nome = "Alice";
+cliente2.cpf = 8882223309;
+cliente2.agencia =  1001;
+cliente2.saldo = 0;
 
-console.log(cliente1Nome);
+cliente3.nome = "Alice";
+cliente3.cpf = 8882223309;
+cliente3.agencia =  1001;
+cliente3.saldo = 0;
+
+console.log(cliente1, cliente2,cliente3);
 
