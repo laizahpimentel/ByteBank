37d6f306c76f7e5870714dbeba7241ce8baa61ac
laizahpimentel <laizahpimentel@gmail.com>
2021-09-18 00:11:09 -0300

    comportamento de class

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
+class ContaCorrente{
+    agencia;
+    saldo;
 
+    sacar(valor){
+        if(this.saldo  >= valor){
+            this.saldo -= valor;
+        }   
+    }
+
+}
+
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 1112223309;
-cliente1.agencia =  1001;
-cliente1.saldo = 0;
 cliente1.rg = 123456789;
 
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 8882223309;
-cliente2.agencia =  1001;
-cliente2.saldo = 0;
+cliente2.rg = 321654987;
 
-cliente3.nome = "Alice";
-cliente3.cpf = 8882223309;
-cliente3.agencia =  1001;
-cliente3.saldo = 0;
 
-console.log(cliente1, cliente2,cliente3);
+const contaCorrenteRicardo = new  ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo.saldo);
+console.log(cliente1);
+console.log(cliente2);
+
+
 
