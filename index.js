f3796744208121938b2e5137bfb4dd1bda2151b3
laizahpimentel <laizahpimentel@gmail.com>
2021-09-29 21:45:39 -0300

    atributos static

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ import {ContaCorrente} from "./ContaCorrente.js"
 const cliente1 = new Cliente("Ricardo",1112223309);
 const cliente2 = new Cliente("Alice",8882223309);
 
-
 const contaCorrenteRicardo = new  ContaCorrente(1001,cliente1);
-contaCorrenteRicardo.depositar(500);
 
+
+contaCorrenteRicardo.depositar(500);
 const conta2 = new ContaCorrente(102, cliente2);
+
 
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
 
+
+console.log(ContaCorrente.numeroDeContas);
 
 
